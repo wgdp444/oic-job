4a375acfadf6b7d7753cdabf7c66fdccdb637306
wgdp444 <tatsuyori.shiraga@triple-four.com>
2020-04-22 01:04:43 +0000

    vue環境ととのえーた

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-content>
+    <router-link to="/header">
+      about
+    </router-link>
+    </v-content>
+    <v-content>
+      
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HeaderItem from './components/HeaderItem.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  // components: {
+  //   HeaderItem
+  // },
+
+  data: () => ({
+  }),
+};
+</script>
--- oicjob/src/main.js
+++ oicjob/src/main.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import vuetify from './plugins/vuetify';
-import router from './router';
-
 
 Vue.config.productionTip = false
 
 new Vue({
-  vuetify,
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')

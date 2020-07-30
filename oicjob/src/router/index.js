c405f0948a6ef37565a6193502c1765e5d423e7f
wgdp444 <tatsuyori.shiraga@triple-four.com>
2020-07-30 14:08:25 +0900

    update realtime search
    - コロンビア
    - 複数条件の検索システムを整備したよ！！！！！

--- oicjob/src/router/index.js
+++ oicjob/src/router/index.js
@@ -55,10 +55,11 @@ const router = new VueRouter({
 router.beforeEach((to, from, next) => {
   console.log(1)
   Vue.GoogleAuth.then(auth2 => {
-    // googleログインしていない場合 クソほど条件長くなったので要改善！！！！！！！！！！！！！！！！！！
     if (to.matched.some(record => (!record.meta.isPublic && !record.meta.isBeforeCreateAccount)) && (!auth2.isSignedIn.get() || !localStorage.getItem('access_token'))) {
+      // googleログインしてない and tokenないばあい クソほど条件長くなったので要改善！！！！！！！！！！！！！！！！！！
       next({ path: '/login', query: { redirect: to.fullPath }});
     } else if (to.matched.some(record => record.meta.isBeforeCreateAccount) && localStorage.getItem('access_token') && auth2.isSignedIn.get()) {
+      // 一度でもユーザー登録したら登録画面には行かない
       next({ path: '/', query: { redirect: to.fullPath }});
     } else {
       next();
